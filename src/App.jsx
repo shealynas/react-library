f0ce5c5a8157ed7020cd83e047440dc20b5f2530
Shealyn Sugai <shealyn@student.dev-island.org>
2024-02-22 20:26:40 -1000

    setup overall layout and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>.library</h1>
+      <div className="book-info">
+        <h3>Title</h3>
+        <p>Author</p>
+        <img src="" alt="" />
+        <p>Description</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
